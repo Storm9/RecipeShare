70dd65eeb16f3b4a664494dfc88d5ffb46e07b49
Kristoffe Zehr <kristoffez@slalom.com>
2013-06-27 15:37:05 -0500

    added IngredientController for adding and updating ingredients

---
+++ RecipeShare/RecipeShare/Scripts/edit-recipe.js
@@ -0,0 +1,13 @@
+﻿jQuery(function($) {
+    function getIngredientEditor(ingredientId) {
+        $.ajax('Ingredient/Edit/' + ingredientId,
+            function(data) {
+                $('.ingredients-list').append(data);
+            },
+            'html');
+    }
+});
+
+jQuery(document).ready(function () {
+    
+});
