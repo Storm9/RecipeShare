f33e50d482a9c08ea1634cd08f0f6238a563d4e8
Kristoffe Zehr <kristoffez@slalom.com>
2013-06-28 12:10:39 -0500

    Using ajax to call the IngredientController from Recipe Edit view

--- RecipeShare/RecipeShare/Scripts/edit-recipe.js
+++ RecipeShare/RecipeShare/Scripts/edit-recipe.js
@@ -1,4 +1,34 @@
-﻿jQuery(document).ready(function () {
+﻿function submitChanges() {
+
+    // submit changes to recipe
+    $.ajax({
+        type: 'POST',
+        url: $('.main-content form').attr('action'),
+        data: {
+            __RequestVerificationToken: $('.main-content form input:[name="__RequestVerificationToken"]:first').val(),
+            Name: $('.main-content form input:[name="Name"]').val(),
+            Instructions: $('.main-content form input:[name="Instructions"]').val()
+        }
+    });
+
+    // sumbint changes to ingredients
+    $('.ingredient-editor').each(function () {
+        $.ajax({
+            type: 'POST',
+            url: '/Ingredient/Edit/' + $(this).data('ingredient'),
+            data: $.map($(this).find('input'), function (input, i) {
+                var retval = {};
+                retval[$(input).attr('name')] = $(input).val();
+                return retval;
+            }).reduce(function (previousValue, currentValue, index, array) {
+                return $.extend(previousValue, currentValue);
+            })
+        });
+    });
+
+}
+
+jQuery(document).ready(function () {
     $('.ingredient-editor').each(function () {
         $(this).load('/Ingredient/Edit/' + $(this).data('ingredient'));
     });
